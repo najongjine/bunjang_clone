5bee295de69293928b9a1444c5eedb54c8d8c0c4
najongjine <najongjin3@hotmail.com>
2025-06-02 12:10:30 +0900

    .

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,13 +5,13 @@ import { Outlet } from "react-router-dom";
 
 const Layout: React.FC = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main>
-        <Outlet /> {/* 각 페이지 내용이 여기 들어옴 */}
+      <main className="flex-grow bg-gray-50 mt-4 py-4">
+        <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
